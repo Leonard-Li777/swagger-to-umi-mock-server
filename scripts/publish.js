ee95ec0572797457825caca49393eae74e42d546
lilun <lilun@beisen.com>
2020-07-21 16:43:39 +0800

    fix: 修复发布脚本

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,30 +1,34 @@
 #!/usr/bin/env node
 
-const shell = require('shelljs');
-const { join } = require('path');
-const { fork } = require('child_process');
+const shell = require('shelljs')
+const { join } = require('path')
+const { fork } = require('child_process')
 
-if (!shell.exec('npm config get registry').stdout.includes('https://registry.npmjs.org/')) {
-  console.error('Failed: set npm registry to https://registry.npmjs.org/ first');
-  process.exit(1);
+if (
+  !shell
+    .exec('npm config get registry')
+    .stdout.includes('http://registry.npmjs.org/')
+) {
+  console.error('Failed: set npm registry to http://registry.npmjs.org/ first')
+  process.exit(1)
 }
 
-const cwd = process.cwd();
-const ret = shell.exec('./node_modules/.bin/lerna updated').stdout;
+const cwd = process.cwd()
+const ret = shell.exec('./node_modules/.bin/lerna updated').stdout
 const updatedRepos = ret
   .split('\n')
   .map(line => line.replace('- ', ''))
-  .filter(line => line !== '');
+  .filter(line => line !== '')
 
 if (updatedRepos.length === 0) {
-  console.log('No package is updated.');
-  process.exit(0);
+  console.log('No package is updated.')
+  process.exit(0)
 }
 
-const { code: buildCode } = shell.exec('npm run build');
+const { code: buildCode } = shell.exec('npm run build')
 if (buildCode === 1) {
-  console.error('Failed: npm run build');
-  process.exit(1);
+  console.error('Failed: npm run build')
+  process.exit(1)
 }
 
 const cp = fork(
@@ -34,31 +38,35 @@ const cp = fork(
     stdio: 'inherit',
     cwd: process.cwd(),
   },
-);
+)
 cp.on('error', err => {
-  console.log(err);
-});
+  console.log(err)
+})
 cp.on('close', code => {
-  console.log('code', code);
+  console.log('code', code)
   if (code === 1) {
-    console.error('Failed: lerna publish');
-    process.exit(1);
+    console.error('Failed: lerna publish')
+    process.exit(1)
   }
 
-  publishToNpm();
-});
+  publishToNpm()
+})
 
 function publishToNpm() {
-  console.log(`repos to publish: ${updatedRepos.join(', ')}`);
+  console.log(`repos to publish: ${updatedRepos.join(', ')}`)
   updatedRepos.forEach(repo => {
-    shell.cd(join(cwd, 'packages', repo));
-    const { version } = require(join(cwd, 'packages', repo, 'package.json'));
-    if (version.includes('-rc.') || version.includes('-beta.') || version.includes('-alpha.')) {
-      console.log(`[${repo}] npm publish --tag next`);
-      shell.exec(`npm publish --tag next`);
+    shell.cd(join(cwd, 'packages', repo))
+    const { version } = require(join(cwd, 'packages', repo, 'package.json'))
+    if (
+      version.includes('-rc.') ||
+      version.includes('-beta.') ||
+      version.includes('-alpha.')
+    ) {
+      console.log(`[${repo}] npm publish --tag next`)
+      shell.exec(`npm publish --tag next`)
     } else {
-      console.log(`[${repo}] npm publish`);
-      shell.exec(`npm publish`);
+      console.log(`[${repo}] npm publish`)
+      shell.exec(`npm publish`)
     }
-  });
+  })
 }
